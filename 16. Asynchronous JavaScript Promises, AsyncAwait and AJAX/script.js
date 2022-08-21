bf040ed9b9b38c5fa5ee7afc39c581512b49bc4a
unknown <sharmag8121@gmail.com>
2022-08-21 08:38:42 -0700

    first ajax call

--- 16. Asynchronous JavaScript Promises, AsyncAwait and AJAX/script.js
+++ 16. Asynchronous JavaScript Promises, AsyncAwait and AJAX/script.js
@@ -3,4 +3,11 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-// ? Ajax
+// ? first Ajax
+const request = new XMLHttpRequest();
+request.open('GET','https://restcountries.com/v3.1/name/india');
+request.send();
+
+request.addEventListener('load',function(){
+    console.log(this.responseText);
+});
